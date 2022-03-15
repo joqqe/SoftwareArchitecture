09519861fa8f9f3699e2a9b9e8ec80ff3579a9d0
Jochen Schoonjans <jochenschoonjans@hotmail.com>
2022-03-15 13:10:43 +0100

    test: finish integration tests

--- SoftwareArchitecture/SoftwareArchitecture.Application.IntegrationTests/TodoItems/TodoItemServiceTests.cs
+++ SoftwareArchitecture/SoftwareArchitecture.Application.IntegrationTests/TodoItems/TodoItemServiceTests.cs
@@ -14,13 +14,62 @@ namespace SoftwareArchitecture.Application.IntegrationTests.TodoItems
         {
             var todoService = (ITodoItemService)serviceProvider.GetService(typeof(ITodoItemService));
 
-            var titleNewTodo = "Test todo";
-            var newId = await todoService.CreateTodo(titleNewTodo);
+            var newTitle = "Test todo";
+            var newId = await todoService.CreateTodo(newTitle);
             
             var actual = (await todoService.GetTodos())
                 .FirstOrDefault(x => x.Id == newId);
 
-            Assert.AreSame(titleNewTodo, actual.Title);
+            Assert.AreSame(newTitle, actual.Title);
+        }
+
+        [Test]
+        public async Task ShouldUpdateTitleOfTodo()
+        {
+            var todoService = (ITodoItemService)serviceProvider.GetService(typeof(ITodoItemService));
+
+            var initialTitle = "Test todo 1";
+            var newId = await todoService.CreateTodo(initialTitle);
+
+            var newTitle = "Test todo 2";
+            await todoService.UpdateTodoTitle(newId, newTitle);
+
+            var actual = (await todoService.GetTodos())
+                .FirstOrDefault(x => x.Id == newId);
+
+            Assert.AreSame(newTitle, actual.Title);
+        }
+
+        [Test]
+        public async Task ShouldUpdateDoneOfTodo()
+        {
+            var todoService = (ITodoItemService)serviceProvider.GetService(typeof(ITodoItemService));
+
+            var initialTitle = "Test todo";
+            var newId = await todoService.CreateTodo(initialTitle);
+
+            await todoService.UpdateTodoDone(newId, true);
+
+            var actual = (await todoService.GetTodos())
+                .FirstOrDefault(x => x.Id == newId);
+
+            Assert.IsTrue(actual.Done);
+        }
+
+        [Test]
+        public async Task ShouldUpdatePriorityOfTodo()
+        {
+            var todoService = (ITodoItemService)serviceProvider.GetService(typeof(ITodoItemService));
+
+            var initialTitle = "Test todo";
+            var newId = await todoService.CreateTodo(initialTitle);
+
+            await todoService.UpdateTodoPriority(newId, Domain.Enums.PriorityLevel.High);
+
+            var actual = (await todoService.GetTodos())
+                .FirstOrDefault(x => x.Id == newId);
+
+            Assert.AreEqual(Domain.Enums.PriorityLevel.High, actual.Priority);
         }
     }
 }
